4bbd307e154cbd4134536b10c981d4691e1ab06f
westcrime <westcrime77@rambler.ru>
2024-12-02 13:40:05 +0100

    [NIS-13912] implemented generic repository

--- Services/Services.Infrastructure/Contexts/AppDbContext.cs
+++ Services/Services.Infrastructure/Contexts/AppDbContext.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Services.Domain.Entities;
 
 namespace Services.Infrastructure.Contexts
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<ServiceCategory> ServiceCategories { get; set; }
+        public DbSet<Specialization> Specializations { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
     }
 }
--- Services/Services.Infrastructure/Repositories/GenericRepository.cs
+++ Services/Services.Infrastructure/Repositories/GenericRepository.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Services.Application.Repositories.Abstractions;
+using Services.Domain.Entities;
+using Services.Domain.Exceptions;
+using Services.Infrastructure.Contexts;
 
 namespace Services.Infrastructure.Repositories
 {
-    internal class GenericRepository
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
     {
+        private readonly AppDbContext _context;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(T entity, CancellationToken cancellationToken)
+        {
+            ArgumentNullException.ThrowIfNull(entity);
+
+            await _context.Set<T>().AddAsync(entity, cancellationToken);
+        }
+
+        public async Task DeleteAsync(Guid id, CancellationToken cancellationToken)
+        {
+            var entityToDelete = await _context.Set<T>().FindAsync(id, cancellationToken) ?? throw new NotFoundException($"Entity with id: {id} not found. Can't delete.");
+            _context.Set<T>().Remove(entityToDelete);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            return (IEnumerable<T>)_context.Set<T>().ToListAsync(cancellationToken);
+        }
+
+        public async Task<T> GetAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return await _context.Set<T>().FindAsync(id, cancellationToken) ?? throw new NotFoundException($"Entity with id: {id} not found.");
+        }
+
+        public async Task UpdateAsync(T entity, CancellationToken cancellationToken)
+        {
+            var entityToUpdate = await _context.Set<T>().FindAsync(entity.Id, cancellationToken) ?? throw new NotFoundException($"Entity with id: {entity.Id} not found. Can't update.");
+            _context.Set<T>().Update(entity);
+        }
     }
 }
