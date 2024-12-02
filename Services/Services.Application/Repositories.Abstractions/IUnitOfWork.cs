2b24727ea91ae2f9d4c3d6a3a3a23bb00f21ed90
westcrime <westcrime77@rambler.ru>
2024-12-02 13:18:40 +0100

    [NIS-13912] added unit of work interface methods

--- Services/Services.Application/Repositories.Abstractions/IUnitOfWork.cs
+++ Services/Services.Application/Repositories.Abstractions/IUnitOfWork.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Services.Domain.Entities;
 
 namespace Services.Application.Repositories.Abstractions
 {
-    internal class IUnitOfWork
+    public interface IUnitOfWork
     {
+        IGenericRepository<T> GetRepository<T>() where T : BaseEntity;
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task RollbackTransactionAsync();
     }
 }
