3b7d9b69fbad884b07f171fbbf8ba17edde50f59
Dzmitriy Talstoi <90412490+westcrime@users.noreply.github.com>
2024-12-12 11:28:25 +0100

    [NIS-13915] implemented domain layer

---
+++ Authorization/Authorization.Domain/Entities/Account.cs
@@ -0,0 +1,37 @@
+﻿using Authorization.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authorization.Domain.Entities
+{
+    public class Account : BaseEntity
+    {
+        [Required]
+        public string Email { get; set; }
+        
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public bool IsEmailVerified { get; set; } = false;
+
+        [Required]
+        public Roles Role { get; set; }
+
+        [Required]
+        public Guid CreatedBy { get; set; }
+
+        [Required]
+        public Guid UpdatedBy { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        [Required]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
