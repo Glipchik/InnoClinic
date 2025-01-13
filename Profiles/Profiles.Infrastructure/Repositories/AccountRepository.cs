a899cb6e8afd0864ea6668ccd9b4728995a8a9c1
Dzmitriy Talstoi <90412490+westcrime@users.noreply.github.com>
2025-01-13 09:37:47 +0100

    [NIS-13910] Fixed account repository

--- Profiles/Profiles.Infrastructure/Repositories/AccountRepository.cs
+++ Profiles/Profiles.Infrastructure/Repositories/AccountRepository.cs
@@ -10,19 +10,55 @@ using System.Threading.Tasks;
 
 namespace Profiles.Infrastructure.Repositories
 {
-    public class AccountRepository : GenericRepository<Account>, IAccountRepository
+    public class AccountRepository : IAccountRepository
     {
         private readonly AppDbContext _context;
 
-        public AccountRepository(AppDbContext context) : base(context)
+        public AccountRepository(AppDbContext context)
         {
             _context = context;
+        }
+
+        public async Task CreateAsync(Account entity, Guid authorId, CancellationToken cancellationToken)
+        {
+            ArgumentNullException.ThrowIfNull(entity);
+
+            entity.CreatedAt = DateTime.UtcNow;
+            entity.CreatedBy = authorId;
+            entity.UpdatedAt = DateTime.UtcNow;
+            entity.UpdatedBy = authorId;
+
+            await _context.Set<Account>().AddAsync(entity, cancellationToken);
+        }
+
+        public async Task DeleteAsync(Guid id, Guid authorId, CancellationToken cancellationToken)
+        {
+            var entityToDelete = await _context.Set<Account>().FindAsync(id);
+            _context.Set<Account>().Remove(entityToDelete);
         }
 
         public async Task<Account> FindByEmailAsync(string email, CancellationToken cancellationToken)
         {
             return await _context.Set<Account>().AsNoTracking()
                .FirstOrDefaultAsync(a => a.Email.Equals(email), cancellationToken: cancellationToken);
+        }
+
+        public async Task<IEnumerable<Account>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            return await _context.Set<Account>().AsNoTracking().ToListAsync(cancellationToken);
+        }
+
+        public async Task<Account> GetAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return await _context.Set<Account>().FindAsync(id);
+        }
+
+        public async Task UpdateAsync(Account entity, Guid authorId, CancellationToken cancellationToken)
+        {
+            entity.UpdatedAt = DateTime.UtcNow;
+            entity.UpdatedBy = authorId;
+
+            _context.Set<Account>().Update(entity);
         }
     }
 }
