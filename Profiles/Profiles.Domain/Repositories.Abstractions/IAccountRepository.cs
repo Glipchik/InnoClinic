402bbcf744e95a50f9d135d65807c7c2bbf373e4
Dzmitriy Talstoi <90412490+westcrime@users.noreply.github.com>
2025-01-13 09:27:08 +0100

    [NIS-13910] Fixed account repository

--- Profiles/Profiles.Domain/Repositories.Abstractions/IAccountRepository.cs
+++ Profiles/Profiles.Domain/Repositories.Abstractions/IAccountRepository.cs
@@ -7,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace Profiles.Domain.Repositories.Abstractions
 {
-    public interface IAccountRepository : IGenericRepository<Account>
+    public interface IAccountRepository
     {
+        Task<IEnumerable<Account>> GetAllAsync(CancellationToken cancellationToken);
+        Task<Account> GetAsync(Guid id, CancellationToken cancellationToken);
+        Task UpdateAsync(Account entity, Guid authorId, CancellationToken cancellationToken);
+        Task DeleteAsync(Guid id, Guid authorId, CancellationToken cancellationToken);
+        Task CreateAsync(Account entity, Guid authorId, CancellationToken cancellationToken);
     }
 }
