5f2c1985ea978053fe74947ee228e4fd768890c8
Dzmitriy Talstoi <90412490+westcrime@users.noreply.github.com>
2025-01-22 13:18:01 +0100

    [NIS-13910] Fixed null warnings

--- Profiles/Profiles.Application/Services/AuthorizationService.cs
+++ Profiles/Profiles.Application/Services/AuthorizationService.cs
@@ -26,10 +26,10 @@ namespace Profiles.Application.Services
         public AuthorizationService(HttpClient httpClient, IConfiguration configuration)
         {
             _httpClient = httpClient;
-            _authorizationServerUrl = configuration["Authorization:ServerUrl"];
-            _authorizationClientUrl = configuration["ProfilesAuthM2M:ServerUrl"];
-            _clientSecret = configuration["ProfilesAuthM2M:ClientSecret"];
-            _clientId = configuration["ProfilesAuthM2M:ClientId"];
+            _authorizationServerUrl = configuration["Authorization:ServerUrl"] ?? throw new ArgumentNullException("Authorization:ServerUrl string is null");
+            _authorizationClientUrl = configuration["ProfilesAuthM2M:ServerUrl"] ?? throw new ArgumentNullException("ProfilesAuthM2M:ServerUrl string is null");
+            _clientSecret = configuration["ProfilesAuthM2M:ClientSecret"] ?? throw new ArgumentNullException("ProfilesAuthM2M:ClientSecret string is null");
+            _clientId = configuration["ProfilesAuthM2M:ClientId"] ?? throw new ArgumentNullException("ProfilesAuthM2M:ClientId string is null");
         }
 
         public async Task<AuthorizationAccountModel> CreateAccount(CreateAccountAuthorizationServerModel createAccountAuthorizationServerModel, CancellationToken cancellationToken)
@@ -56,7 +56,7 @@ namespace Profiles.Application.Services
             var newResponse = await response.Content.ReadAsStringAsync();
 
             var accountModel = await response.Content.ReadFromJsonAsync<AuthorizationAccountModel>(cancellationToken: cancellationToken);
-            return accountModel;
+            return accountModel ?? throw new BadRequestException("Account model is null");
         }
 
         private async Task<string> GetAccessTokenAsync(CancellationToken cancellationToken)
@@ -76,12 +76,12 @@ namespace Profiles.Application.Services
                 throw new Exception("Failed to retrieve access token");
             }
 
-            return tokenResponse.AccessToken;
+            return tokenResponse.AccessToken ?? throw new Exception("Failed to retrieve access token");
         }
 
         private class TokenResponse
         {
-            public string AccessToken { get; set; }
+            public string? AccessToken { get; set; }
         }
     }
 }
