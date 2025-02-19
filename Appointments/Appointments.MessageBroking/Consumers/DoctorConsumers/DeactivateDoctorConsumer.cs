609815fe098d219670adf488d32895b3fd8828a7
Dzmitriy Talstoi <90412490+westcrime@users.noreply.github.com>
2025-02-19 09:28:33 +0100

    [NIS-13992] Fixed project dependencies

--- Appointments/Appointments.MessageBroking/Consumers/DoctorConsumers/DeactivateDoctorConsumer.cs
+++ Appointments/Appointments.MessageBroking/Consumers/DoctorConsumers/DeactivateDoctorConsumer.cs
@@ -8,9 +8,8 @@ namespace Appointments.MessageBroking.Consumers.DoctorConsumers
     public class DeactivateDoctorConsumer(IUnitOfWork unitOfWork) : IConsumer<DoctorDeactivated>
     {
         private readonly IUnitOfWork _unitOfWork = unitOfWork;
-    }
 
-    public async Task Consume(ConsumeContext<DoctorDeactivated> context)
+        public async Task Consume(ConsumeContext<DoctorDeactivated> context)
         {
             await _unitOfWork.DoctorRepository.DeleteAsync(context.Message.Id, CancellationToken.None);
             await _unitOfWork.SaveChangesAsync(CancellationToken.None);
