b2fa7f553c82ba9fbc69cb2aa8331e2e14c87ddb
Dzmitriy Talstoi <90412490+westcrime@users.noreply.github.com>
2025-02-21 15:51:35 +0100

    [NIS-13992] Added use formik context usage

--- react-client/src/features/create-appointment-form/index.tsx
+++ react-client/src/features/create-appointment-form/index.tsx
@@ -1,59 +1,49 @@
-import type React from "react"
-import { useState, useEffect } from "react"
-import { validationSchema } from "./models/validationSchema"
-import { MIN_APPOINTMENT_DATE } from "./lib/dateUtils"
-import DatePicker from "../../shared/ui/forms/DatePicker"
-import { Form, Formik, FormikErrors } from 'formik'
-import { useDispatch, useSelector } from "react-redux"
-import { fetchSpecializationsRequest } from "../../shared/store/specializations"
-import { fetchServicesRequest } from "../../shared/store/services"
-import { fetchDoctorsRequest } from "../../shared/store/doctors"
-import { fetchDoctorScheduleRequest } from "../../shared/store/doctor-schedule"
-import SpecializationSelect from "../../shared/ui/specialization-select"
-import ServiceSelect from "../../shared/ui/service-select"
-import DoctorSelect from "../../shared/ui/doctor-select"
-import TimeSlotSelect from "../../shared/ui/time-slot-select"
-import CreateAppointmentModel from "./models/createAppointmentModel"
-import { createAppointmentRequest } from "./models/store"
-import { RootState } from "../../store"
-import Loading from "../../shared/ui/controls/Loading"
-import ErrorBox from "../../shared/ui/containers/ErrorBox"
-import CancelAndSubmit from "../../shared/ui/widgets/cancel-and-submit"
+import type React from "react";
+import { useState, useEffect } from "react";
+import { useFormikContext } from "formik";
+import { validationSchema } from "./models/validationSchema";
+import { MIN_APPOINTMENT_DATE } from "./lib/dateUtils";
+import DatePicker from "../../shared/ui/forms/DatePicker";
+import { Form, Formik } from "formik";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchSpecializationsRequest } from "../../shared/store/specializations";
+import { fetchServicesRequest } from "../../shared/store/services";
+import { fetchDoctorsRequest } from "../../shared/store/doctors";
+import { fetchDoctorScheduleRequest } from "../../shared/store/doctor-schedule";
+import SpecializationSelect from "../../shared/ui/specialization-select";
+import ServiceSelect from "../../shared/ui/service-select";
+import DoctorSelect from "../../shared/ui/doctor-select";
+import TimeSlotSelect from "../../shared/ui/time-slot-select";
+import CreateAppointmentModel from "./models/createAppointmentModel";
+import { createAppointmentRequest } from "./models/store";
+import { RootState } from "../../store";
+import Loading from "../../shared/ui/controls/Loading";
+import ErrorBox from "../../shared/ui/containers/ErrorBox";
+import CancelAndSubmit from "../../shared/ui/widgets/cancel-and-submit";
 
 interface CreateAppointmentFormProps {
-  onCancel: () => void
+  onCancel: () => void;
 }
 
 interface CreateAppointmentFormModel {
-  specializationId: string,
-  doctorId: string,
-  serviceId: string,
-  timeSlotId: number,
-  date: string
+  specializationId: string;
+  doctorId: string;
+  serviceId: string;
+  timeSlotId: number;
+  date: string;
 }
 
 const CreateAppointmentForm = ({ onCancel }: CreateAppointmentFormProps) => {
   const dispatch = useDispatch();
   const [date, setDate] = useState<string | null>(null);
-  const { loading, error } = useSelector(
-    (state: RootState) => state.createAppointment
-  );
+  const { loading, error } = useSelector((state: RootState) => state.createAppointment);
+  const { values, touched, errors, setFieldValue, handleChange } = useFormikContext<CreateAppointmentFormModel>();
 
   useEffect(() => {
     dispatch(fetchSpecializationsRequest());
   }, [dispatch]);
 
-  const initialValues: CreateAppointmentFormModel = {
-    serviceId: "",
-    specializationId: "",
-    date: MIN_APPOINTMENT_DATE.toISOString().split("T")[0],
-    timeSlotId: -1,
-    doctorId: "",
-  };
-
-  const handleSpecializationChange = (e: React.ChangeEvent<HTMLSelectElement>, 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    setFieldValue: (field: string, value: any, shouldValidate?: boolean) => Promise<void | FormikErrors<CreateAppointmentFormModel>>) => {
+  const handleSpecializationChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const specializationId = e.target.value;
     setFieldValue("specializationId", specializationId, true);
     if (specializationId) {
@@ -62,9 +52,7 @@ const CreateAppointmentForm = ({ onCancel }: CreateAppointmentFormProps) => {
     }
   };
 
-  const handleDoctorChange = (e: React.ChangeEvent<HTMLSelectElement>, 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    setFieldValue: (field: string, value: any, shouldValidate?: boolean) => Promise<void | FormikErrors<CreateAppointmentFormModel>>) => {
+  const handleDoctorChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const doctorId = e.target.value;
     setFieldValue("doctorId", doctorId, true);
     if (doctorId && date) {
@@ -72,15 +60,12 @@ const CreateAppointmentForm = ({ onCancel }: CreateAppointmentFormProps) => {
     }
   };
 
-  const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>, 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    setFieldValue: (field: string, value: any, shouldValidate?: boolean) => Promise<void | FormikErrors<CreateAppointmentFormModel>>,
-    doctorId: string) => {
+  const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const selectedDate = e.target.value;
     setDate(selectedDate);
     setFieldValue("date", selectedDate, true);
-    if (doctorId && selectedDate) {
-      dispatch(fetchDoctorScheduleRequest({ doctorId, date: new Date(selectedDate) }));
+    if (values.doctorId && selectedDate) {
+      dispatch(fetchDoctorScheduleRequest({ doctorId: values.doctorId, date: new Date(selectedDate) }));
     }
   };
 
@@ -89,58 +74,62 @@ const CreateAppointmentForm = ({ onCancel }: CreateAppointmentFormProps) => {
   };
 
   return (
-    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
-      {({ values, touched, errors, handleChange, setFieldValue }) => (
-        <Form className="flex w-[40%] flex-col gap-6 p-6 bg-white shadow-lg rounded-lg max-w-lg m-6">
-          <SpecializationSelect
-            disabled={false}
-            id="specialization-select-for-create-appointment-form-id"
-            name="specializationId"
-            value={values.specializationId}
-            onChange={(e) => handleSpecializationChange(e, setFieldValue)}
-            error={touched.specializationId ? errors.specializationId : undefined}
-          />
-          <ServiceSelect
-            id="service-select-for-create-appointment-form-id"
-            disabled={values.specializationId ? false : true}
-            name="serviceId"
-            onChange={handleChange}
-            value={values.serviceId}
-            className={values.specializationId ? "" : "opacity-50 cursor-not-allowed"}
-            error={touched.serviceId ? errors.serviceId : undefined}
-          />
-          <DoctorSelect 
-            id="doctor-select-for-create-appointment-form-id"
-            disabled={values.specializationId ? false : true}
-            name="doctorId"
-            onChange={(e) => handleDoctorChange(e, setFieldValue)}
-            value={values.doctorId}
-            className={values.specializationId ? "" : "opacity-50 cursor-not-allowed"}
-            error={touched.doctorId ? errors.doctorId : undefined}
-          />
-          <DatePicker
-            label="Choose a date for the appointment"
-            id="select-date-for-create-appointment-form-id"
-            name="date"
-            value={values.date}
-            onChange={(e) => handleDateChange(e, setFieldValue, values.doctorId)}
-            disabled={false}
-            error={touched.date ? errors.date : undefined}
-          />
-          <TimeSlotSelect
-            id="time-slot-select-for-create-appointment-form-id"
-            disabled={(values.date && values.doctorId) ? false : true}
-            name="timeSlotId"
-            onChange={handleChange}
-            value={values.timeSlotId}
-            className={(values.date && values.doctorId) ? "" : "opacity-50 cursor-not-allowed"}
-            error={touched.timeSlotId ? errors.timeSlotId : undefined}
-          />
-          <CancelAndSubmit onCancel={onCancel} /> 
-          {loading && <Loading label="Loading specializations..." />}
-          {error && <ErrorBox value={error}></ErrorBox>}
-        </Form>
-      )}
+    <Formik initialValues={{
+      serviceId: "",
+      specializationId: "",
+      date: MIN_APPOINTMENT_DATE.toISOString().split("T")[0],
+      timeSlotId: -1,
+      doctorId: "",
+    }} validationSchema={validationSchema} onSubmit={onSubmit}>
+      <Form className="flex w-[40%] flex-col gap-6 p-6 bg-white shadow-lg rounded-lg max-w-lg m-6">
+        <SpecializationSelect
+          disabled={false}
+          id="specialization-select-for-create-appointment-form-id"
+          name="specializationId"
+          value={values.specializationId}
+          onChange={handleSpecializationChange}
+          error={touched.specializationId ? errors.specializationId : undefined}
+        />
+        <ServiceSelect
+          id="service-select-for-create-appointment-form-id"
+          disabled={values.specializationId ? false : true}
+          name="serviceId"
+          onChange={handleChange}
+          value={values.serviceId}
+          className={values.specializationId ? "" : "opacity-50 cursor-not-allowed"}
+          error={touched.serviceId ? errors.serviceId : undefined}
+        />
+        <DoctorSelect 
+          id="doctor-select-for-create-appointment-form-id"
+          disabled={values.specializationId ? false : true}
+          name="doctorId"
+          onChange={handleDoctorChange}
+          value={values.doctorId}
+          className={values.specializationId ? "" : "opacity-50 cursor-not-allowed"}
+          error={touched.doctorId ? errors.doctorId : undefined}
+        />
+        <DatePicker
+          label="Choose a date for the appointment"
+          id="select-date-for-create-appointment-form-id"
+          name="date"
+          value={values.date}
+          onChange={handleDateChange}
+          disabled={false}
+          error={touched.date ? errors.date : undefined}
+        />
+        <TimeSlotSelect
+          id="time-slot-select-for-create-appointment-form-id"
+          disabled={(values.date && values.doctorId) ? false : true}
+          name="timeSlotId"
+          onChange={handleChange}
+          value={values.timeSlotId}
+          className={(values.date && values.doctorId) ? "" : "opacity-50 cursor-not-allowed"}
+          error={touched.timeSlotId ? errors.timeSlotId : undefined}
+        />
+        <CancelAndSubmit onCancel={onCancel} /> 
+        {loading && <Loading label="Loading specializations..." />}
+        {error && <ErrorBox value={error}></ErrorBox>}
+      </Form>
     </Formik>
   );
 };
