1423fd8c9b01d2ce613df0a8c86c84a80e226b18
Dzmitriy Talstoi <westcrime7777@gmail.com>
2025-03-04 18:48:30 +0300

    [NIS-13992] Made requested changes

--- react-client/src/features/create-office-form/ui/index.tsx
+++ react-client/src/features/create-office-form/ui/index.tsx
@@ -1,20 +1,38 @@
-import Button from "@shared/ui/controls/Button";
-import { useState } from "react";
-import { CreateOfficeForm } from "./form";
+import { Formik } from "formik";
+import { useDispatch } from "react-redux";
+import { CreateOfficeModel } from "../models/createOfficeModel";
+import { validationSchema } from "../models/validationSchema";
+import { createOfficeRequest } from "../store/create-office";
+import InnerForm from "./inner-form";
 
-export const CreateOffice = () => {
-  const [isCreating, setIsCreating] = useState<boolean>(false);
-  
-  return (
-    <>
-      <div className="flex justify-end m-4">
-        <Button onClick={() => setIsCreating(true)}>
-          Create New Office
-        </Button>
-      </div>
-      <div className="flex flex-col items-center">
-        {isCreating && <CreateOfficeForm close={() => setIsCreating(false)} />}
-      </div>
-    </>
-  )
+interface CreateOfficeFormProps {
+  close: () => void;
 }
+
+export interface CreateOfficeFormModel {
+  address: string,
+  registryPhoneNumber: string,
+  isActive: boolean
+}
+
+const CreateOfficeForm = ({ close }: CreateOfficeFormProps) => {
+  const dispatch = useDispatch();
+
+  const onSubmit = (values: CreateOfficeModel) => {
+    dispatch(createOfficeRequest(values));
+  };
+
+  const initialValues: CreateOfficeModel = {
+    address: "",
+    isActive: true,
+    registryPhoneNumber: ""
+  }
+
+  return (
+    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+      <InnerForm close={close} />
+    </Formik>
+  );
+};
+
+export { CreateOfficeForm };
