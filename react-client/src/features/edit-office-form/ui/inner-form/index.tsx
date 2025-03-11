fd24db775c1fc4f7b34e73f3e7b78c16d634c35b
Dzmitriy Talstoi <90412490+westcrime@users.noreply.github.com>
2025-03-11 09:38:30 +0100

    [NIS-13992] Fix form cancelling and redux actions

--- react-client/src/features/edit-office-form/ui/inner-form/index.tsx
+++ react-client/src/features/edit-office-form/ui/inner-form/index.tsx
@@ -2,31 +2,21 @@ import { RootState } from "@app/store";
 import Label from "@shared/ui/containers/Label";
 import Loading from "@shared/ui/controls/Loading";
 import { Form, useFormikContext } from "formik";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import FormFooter from "@widgets/form-footer";
 import Input from "@shared/ui/forms/Input";
 import { EditOfficeModel } from "@features/edit-office-form/models/editOfficeModel";
-import { useEffect } from "react";
-import { resetState } from "@features/edit-office-form/store/edit-office";
 import Checkbox from "@shared/ui/forms/CheckBox";
 
 const InnerForm = () => {
   const { values, touched, errors, handleChange, handleBlur } = useFormikContext<EditOfficeModel>();
   const { loading, error, success } = useSelector((state: RootState) => state.editOffice);
   const navigate = useNavigate()
-  const dispatch = useDispatch()
 
   const handleCancel = () => {
     navigate(-1)
   }
-  
-  useEffect(() => {
-    if (success) {
-      dispatch(resetState())
-      navigate(-1)
-    }
-  }, [success, navigate]);
 
   return (
     <Form className="flex w-[40%] flex-col gap-6 p-6 bg-white shadow-lg rounded-lg max-w-lg m-6">
