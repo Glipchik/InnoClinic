f0d52dba6b293a6c940478fcd0960af1e60423ce
Dzmitriy Talstoi <90412490+westcrime@users.noreply.github.com>
2025-03-28 14:00:09 +0100

    [NIS-13992] Fix id usage

--- react-client/cypress.config.mjs
+++ react-client/cypress.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      config.env.BASE_URL = process.env.VITE_BASE_URL;
-      config.env.AUTH_SERVER_BASE_URL = process.env.VITE_AUTH_SERVER_BASE_URL;
+      config.env.BASE_URL = "http://localhost:3000";
+      config.env.AUTH_SERVER_BASE_URL = "https://localhost:5001";
       return config;
     },
     baseUrl: "http://localhost:3000",
